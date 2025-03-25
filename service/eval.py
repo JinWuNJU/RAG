1dcb3a410bda0c3933c1d531f35f66362961a233
朗昱 韦 <221250011@smail.nju.edu.cn>
2025-03-25 11:08:44 +0800

    移动eval.py并修正ChatOpenAI参数名字

--- service/eval.py
+++ service/eval.py
@@ -6,8 +6,8 @@ from ragas.llms import LangchainLLMWrapper
 load_dotenv()
 # 查看 https://docs.ragas.io/en/stable/getstarted/evals/#evaluating-using-a-llm-based-metric
 llm = ChatOpenAI(
-        openai_api_base="https://open.bigmodel.cn/api/paas/v4/",
-        openai_api_key=os.getenv("ZHIPU_API_KEY"),
-        model_name="glm-4-flash"
+        base_url="https://open.bigmodel.cn/api/paas/v4/",
+        api_key=os.getenv("ZHIPU_API_KEY"),
+        model="glm-4-flash"
 )
 evaluator_llm = LangchainLLMWrapper(llm)
